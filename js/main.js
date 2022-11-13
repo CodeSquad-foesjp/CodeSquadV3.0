1fafce3554d0d90bab23fac308121288193df246
CodeSquad-foesjp <76586717+CodeSquad-foesjp@users.noreply.github.com>
2022-11-14 00:59:59 +0530

    js

--- js/main.js
+++ js/main.js
@@ -150,7 +150,7 @@
 		var timeinterval = setInterval(updateClock, 1000);
 	}
 
-	var deadline = "November 13,2022 23:59:59"//new Date(Date.parse(new Date()) + 6 * 24 * 60 * 60 * 1000);
+	var deadline = "November 19,2022 18:00:00"//new Date(Date.parse(new Date()) + 6 * 24 * 60 * 60 * 1000);
 	initializeClock('clockdiv', deadline);
 
 	/*----------------------------------------------------*/
